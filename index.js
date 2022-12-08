5596dd062c6de2c74710777d2979c26498aa2909
Badjarda <mestredua@gmail.com>
2022-12-08 12:03:03 -0500

    certs

--- index.js
+++ index.js
@@ -22,38 +22,3 @@ connection.query('SELECT * FROM Client', function (error, results, fields) {
 });
 
 connection.end();
-// connection.connect((err) => {
-//   if (err) throw err;
-//   console.log('Connected to MySQL Server!');
-// });
-
-
-//const port = 3000
-//const valuesRouter = require('./routes/values')
-
-/*app.use(express.json());
-
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-
-app.get('/', (req, res) => {
-  res.json({message: "ok"})
-})
-
-app.use("/values", valuesRouter);
-/* Error handler middleware 
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  console.error(err.message, err.stack);
-  res.status(statusCode).json({ message: err.message });
-  return;
-});
-
-
-app.listen(port, () => {
-  console.log('Example app listening on port ${port}')
-})
-*/
