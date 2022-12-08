2524065f9917a763498c006dcf7b2aa0049304ce
Duarte Costa <mestredua@gmail.com>
2022-12-08 17:04:03 +0000

    index e certs novos

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 const config = {
-  host:"192.168.0.100",
-  user:"nodejs",
-  password:"password",
-  database:"remotedb",
-  debug : true
+    host: "192.168.0.100",
+    user: "nodejs",
+    password: "password",
+    database: "remotedb",
 };
 module.exports = config;
--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var routes = require('./routes/routes');
-
-var app = express();
-app.listen(3019);
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-//app.use('/users', usersRouter);
-app.use('/', routes);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+const express = require('express')
+const app = express()
+const fs = require('fs');
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: "192.168.0.100",
+    user: "nodejs",
+    password: "password",
+    database: "remotedb",
+    ssl: {
+        ca: fs.readFileSync(__dirname + '/certs/ca.pem'),
+        key: fs.readFileSync(__dirname + '/certs/client-key.pem'),
+        cert: fs.readFileSync(__dirname + '/certs/client-cert.pem')
+    }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error', {title: 'Express'});
+connection.connect();
+connection.query('SELECT * FROM Client', function(error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results);
 });
 
-module.exports = app;
+connection.end();
