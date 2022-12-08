63277f3192d0166725e3aa79c263cb95b121ee64
Duarte Costa <mestredua@gmail.com>
2022-12-08 19:23:26 +0000

    Update index.js

--- index.js
+++ index.js
@@ -15,33 +15,26 @@ const connection = mysql.createConnection({
         cert: fs.readFileSync(__dirname + '/certs/client-cert.pem')
     }
 });
-
-
 connection.connect();
-//connection.query('SELECT * FROM Client', function(error, results, fields) {
-  //  if (error) throw error;
-    //console.log('The solution is: ', results);
-//});
-
-//connection.end();
-
 //var cors = require("cors");
 //app.use(cors());
 app.listen(3019);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', routes);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
+    next(createError(404));
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+});
+//connection.end();
 module.exports = app;
