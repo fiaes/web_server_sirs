7a29a8e980e86e8ac83c83baca67a1872ef83c79
Badjarda <mestredua@gmail.com>
2022-12-08 15:01:34 -0500

    Revert "try new requests"
    This reverts commit a5a80dd0c41ddc9dd0d4af9277cd33bc047d49bd.

--- config.js
+++ config.js
@@ -1,16 +1,13 @@
 const fs = require('fs');
 const config = {
-    db: {
-        host: "192.168.0.100",
-        user: "nodejs",
-        password: "password",
-        database: "remotedb",
-        ssl: {
-            ca: fs.readFileSync(__dirname + '/certs/ca.pem'),
-            key: fs.readFileSync(__dirname + '/certs/client-key.pem'),
-            cert: fs.readFileSync(__dirname + '/certs/client-cert.pem')
-        }
-    },
-    listPerPage: 10,
+    host: "192.168.0.100",
+    user: "nodejs",
+    password: "password",
+    database: "remotedb",
+    ssl: {
+        ca: fs.readFileSync(__dirname + '/certs/ca.pem'),
+        key: fs.readFileSync(__dirname + '/certs/client-key.pem'),
+        cert: fs.readFileSync(__dirname + '/certs/client-cert.pem')
+    }
 };
 module.exports = config;
